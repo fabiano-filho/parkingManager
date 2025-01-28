981820be6f2904ac80b75a91b321e6732f8fd8b4
Fabiano Filho <fabianobnfilho09@gmail.com>
2025-01-28 00:21:17 -0300

    FEAT: criar modelo veiculo em models.py

--- parking/models.py
+++ parking/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils.timezone import now
+
 
 # Create your models here.
+class Veiculo(models.Model):
+    placa = models.CharField(max_length=7, unique=True)
+    data_entrada = models.DateTimeField(default=now)
+    data_saida = models.DateTimeField(null=True, blank=True)
+    valor_pago = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True
+    )
+    pago = models.BooleanField(default=False)
+    data_pagamento = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.placa} - Entrada: {self.data_entrada}"
