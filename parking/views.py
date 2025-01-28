11172a59375bc932d2f6a56c4f85bc67d6d641d3
Fabiano Filho <fabianobnfilho09@gmail.com>
2025-01-28 01:29:22 -0300

    FEAT: autenticacao e rotas de token jwt

--- parking/views.py
+++ parking/views.py
@@ -1,14 +1,22 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import (
+    api_view,
+    authentication_classes,
+    permission_classes,
+)
 from django.utils.timezone import now
 from .functions import validar_placa
 from .models import Veiculo
 from .serializers import VeiculoSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 # Create your views here.
 @api_view(["POST"])
+@authentication_classes([JWTAuthentication])  # Define a autenticação JWT
+@permission_classes([IsAuthenticated])
 def registrar_entrada(request):
     placa = request.data.get("placa")
     if not placa:
--- parkingManager/urls.py
+++ parkingManager/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("parking.urls"), name="parking.urls"),
+    path(
+        "api/login",
+        TokenObtainPairView.as_view(),
+    ),
+    path(
+        "api/token/refresh",
+        TokenObtainPairView.as_view(),
+    ),
 ]
